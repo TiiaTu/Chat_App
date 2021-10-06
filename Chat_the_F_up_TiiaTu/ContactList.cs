a2e5e8801c09195043342d17a6cf15138786ed79
Tiia Tupamäki <tiia.tupamaki@gmail.com>
2021-10-06 15:35:40 +0200

    Add project files.

---
+++ Chat_the_F_up_TiiaTu/ContactList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chat_the_F_up_TiiaTu
+{
+    class ContactList : Person
+    {
+        List<string> nameList = new List<string>
+        {
+            Person.
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("Choose the number that indicates the option");
+            Console.WriteLine("1 - Show the list \n2 - Add a person to the list \n3 - Update the list \n4 - Delete a person from the list \n5 - Sort the list");
+            Console.ReadKey();
+            Console.WriteLine("You chose");
+        }
+        static void ChooseAction()
+        {
+            switch (true)
+            {
+                case "1":
+                    Console.WriteLine("Show the list");
+                    nameList
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Chat_the_F_up_TiiaTu/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chat_the_F_up_TiiaTu
+{
+    class Program
+    {
+        static void Main()
+        {
+            ContactList contact = new ContactList();
+
+            ContactList.Menu();
+
+        }
+    }
+}
